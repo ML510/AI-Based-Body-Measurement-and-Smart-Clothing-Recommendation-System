b9d20e41f89a95d351a9f7855fa6f0ca56f2aba1
ML510 <mr.mahesh.303@gmail.com>
2026-05-10 01:52:40 +0530

    Project initializer

---
+++ src/main/java/com/bodymeasure/BodyMeasurementApplication/BodyMeasurementApplication.java
@@ -0,0 +1,13 @@
+package com.bodymeasure.BodyMeasurementApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyMeasurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyMeasurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bodymeasure/BodyMeasurementApplication/BodyMeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodymeasure.BodyMeasurementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyMeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
