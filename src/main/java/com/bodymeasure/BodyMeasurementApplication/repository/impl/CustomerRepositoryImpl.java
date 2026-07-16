44a7ccf0e8d1a7ec52d121e167ba7aa5f686a441
ML510 <mr.mahesh.303@gmail.com>
2026-07-16 14:41:06 +0530

    Customer Save Part

--- src/main/java/com/bodymeasure/BodyMeasurementApplication/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/com/bodymeasure/BodyMeasurementApplication/repository/impl/CustomerRepositoryImpl.java
@@ -2,12 +2,25 @@ package com.bodymeasure.BodyMeasurementApplication.repository.impl;
 
 import com.bodymeasure.BodyMeasurementApplication.model.Customer;
 import com.bodymeasure.BodyMeasurementApplication.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+        private final JdbcTemplate jdbcTemplate;
+
     @Override
     public boolean saveCustomer(Customer customer) {
-        return true;
+        String sql = "INSERT INTO customer (name, email, address, phoneNumber) VALUES (?, ?, ?, ?)";
+
+        return jdbcTemplate.update(sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAddress(),
+                customer.getPhoneNumber()
+        )>0;
     }
 }
